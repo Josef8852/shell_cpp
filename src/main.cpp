eace9d4a786fabeb9cfdb310e265fbb00c40f222
Josef8852 <josefaboshady2@gmail.com>
2026-05-16 20:12:13 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <sstream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <vector>
 #include "shell.h"
 
 using namespace std;
-
 
 
 int main() {
@@ -46,8 +48,57 @@ int main() {
       }
 
       else {
-          cout << command << ": command not found" << endl ;
+
+          string fullPath = findExecutableInPath(getPath(), command) ;
+          
+          if(!fullPath.empty()) {
+
+              string arg ; 
+
+              vector<string> args ; 
+
+              args.push_back(command) ;
+              
+              while(params >> arg) {
+                  args.push_back(arg) ;
+              }
+
+              vector<char*> argv ; 
+
+              // argv accepts char* argv[] in C 
+
+              for(auto &str : args) {
+                  argv.push_back(str.data());
+              }
+
+              // end of args 
+              argv.push_back(nullptr);
+
+              // create a child process 
+              pid_t pid = fork() ;
+
+              // inside child
+              if(pid == 0) {
+                  execv(fullPath.c_str() , argv.data());
+                  perror("execv failed");
+                  exit(1);
+              }
+              // inside parent
+              else if(pid > 0) {
+                  // wait until child finish
+                  waitpid(pid, nullptr , 0) ;
+              }
+              else {
+                  perror("fork failed");
+              }
+          }
+          else {
+              cout << command << ": command not found" << endl ;
+          }
       }
+
+
+
 
   }
 
